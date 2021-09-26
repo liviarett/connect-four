ad3946ce02d70af91dddaa7898eca8982a2e798e
Livia Rett <livia@qubit.com>
2021-09-26 14:24:38 +0100

    Improve css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
-import styled from 'styled-components';
-import Board from './components/board';
-import Dashboard from "./components/dashboard";
-import { areEqual, getValidNeighbours, APP_BACKGROUND_COLOR } from "./helpers";
+import React, { Component } from 'react'
+import './App.css'
+import styled from 'styled-components'
+import Board from './components/board'
+import Dashboard from './components/dashboard'
+import { areEqual, getValidNeighbours, APP_BACKGROUND_COLOR } from './helpers'
 
 const StyledApp = styled.div`
   height: 100vh;
@@ -16,32 +16,37 @@ const StyledApp = styled.div`
   background-color: ${APP_BACKGROUND_COLOR};
   font-family: Roboto, sans-serif;
   line-height: 1.5;
-`;
+  padding: 32px 0;
+`
 
-const WIDTH = 7;
-const HEIGHT = 6;
+const Wrapper = styled.div`
+  max-height: 100%;
+`
+
+const WIDTH = 7
+const HEIGHT = 6
 
 const initialState = {
   currentPlayer: 1,
   board: [],
   winner: null,
-  moves: 0,
-};
+  moves: 0
+}
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = initialState;
+  constructor () {
+    super()
+    this.state = initialState
   }
 
   generateBoard = () => {
-    const board = [];
+    const board = []
     for (let i = 0; i < HEIGHT; i++) {
-      const row = [];
+      const row = []
       for (let k = 0; k < WIDTH; k++) {
-        row.push({ player: 0, address: [i, k]});
+        row.push({ player: 0, address: [i, k] })
       }
-      board.push(row);
+      board.push(row)
     }
 
     this.setState({
@@ -49,102 +54,115 @@ class App extends Component {
     })
   }
 
-  getNextPlayer = (currentPlayer) => {
-    return currentPlayer === 1 ? 2 : 1;
+  getNextPlayer = currentPlayer => {
+    return currentPlayer === 1 ? 2 : 1
   }
 
-  handleClick = (address) => {
-    const {
-      currentPlayer,
-      board,
-      winner,
-      moves,
-    } = this.state;
+  handleClick = address => {
+    const { currentPlayer, board, winner, moves } = this.state
 
     if (winner) {
-      return;
+      return
     }
-    const column = address[1];
-    const boardCopy = board.slice(0);
+    const column = address[1]
+    const boardCopy = board.slice(0)
 
-    let i = HEIGHT - 1;
+    let i = HEIGHT - 1
 
     while (boardCopy[i] && boardCopy[i][column].player !== 0) {
-      i--;
+      i--
     }
 
     if (!boardCopy[i]) {
-      return;
+      return
     }
 
-    const cell = boardCopy[i][column];
+    const cell = boardCopy[i][column]
 
     const updatedCell = {
       ...cell,
-      player: currentPlayer,
+      player: currentPlayer
     }
 
-    boardCopy[i][column] = updatedCell;
+    boardCopy[i][column] = updatedCell
 
     this.setState({
       board: boardCopy,
       currentPlayer: this.getNextPlayer(currentPlayer),
-      moves: moves + 1,
+      moves: moves + 1
     })
 
-    const finishGame = this.isGameFinished(updatedCell, board);
+    const finishGame = this.isGameFinished(updatedCell, board)
 
     if (finishGame) {
       this.setState({
         winner: currentPlayer
-      });
+      })
     }
   }
 
-  setWinner = (player) => {
+  setWinner = player => {
     this.setState({
       winner: player
-    });
+    })
   }
 
   isGameFinished = (cell, board, direction, connected = 1) => {
     if (connected === 4) {
-      this.setWinner(this.state.currentPlayer);
+      this.setWinner(this.state.currentPlayer)
     }
-    const validNeighbours = getValidNeighbours(cell.address, board);
+    const validNeighbours = getValidNeighbours(cell.address, board)
 
     validNeighbours.forEach(neighbour => {
-      if (neighbour.player === cell.player && (!direction || areEqual(direction, neighbour.direction))) {
-        this.isGameFinished(neighbour, board, neighbour.direction, connected + 1);
+      if (
+        neighbour.player === cell.player &&
+        (!direction || areEqual(direction, neighbour.direction))
+      ) {
+        this.isGameFinished(
+          neighbour,
+          board,
+          neighbour.direction,
+          connected + 1
+        )
       }
-    });
-    return false;
+    })
+    return false
   }
 
   restartGame = () => {
     this.setState({
       ...initialState
-    });
-    this.generateBoard();
+    })
+    this.generateBoard()
   }
 
-  componentDidMount() {
-    this.generateBoard();
+  componentDidMount () {
+    this.generateBoard()
   }
 
-  render() {
-    const { currentPlayer, moves, board, winner } = this.state;
+  render () {
+    const { currentPlayer, moves, board, winner } = this.state
     return (
-      <StyledApp className="App">
-        <div>
-        { board.length && <Board board={board} handleClick={this.handleClick} winner={winner} /> }
-        {
-          <Dashboard currentPlayer={currentPlayer} moves={moves} restartGame={this.restartGame}/>
-        }
-        </div>
+      <StyledApp className='App'>
+        <Wrapper>
+          {board.length && (
+            <Board
+              board={board}
+              handleClick={this.handleClick}
+              winner={winner}
+            />
+          )}
+          {
+            <Dashboard
+              currentPlayer={currentPlayer}
+              moves={moves}
+              restartGame={this.restartGame}
+            />
+          }
+        </Wrapper>
       </StyledApp>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Button from "./button";
-import { WithPlayerColor } from "../helpers";
+import React from 'react'
+import Button from './button'
+import { WithPlayerColor } from '../helpers'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  padding-bottom: 32px;
+`
 
 const Dashboard = ({ moves, currentPlayer, restartGame }) => (
+  <Wrapper>
+    <br />
+    <div>Moves: {moves}</div>
+    <br />
     <div>
-      <br />
-      <div>Moves: { moves }</div>
-      <br />
-      <div>Now playing: <WithPlayerColor player={currentPlayer}><b>Player { currentPlayer }</b></WithPlayerColor></div>
-      <Button handleClick={restartGame} text="Restart Game"/>
+      Now playing:{' '}
+      <WithPlayerColor player={currentPlayer}>
+        <b>Player {currentPlayer}</b>
+      </WithPlayerColor>
     </div>
+    <Button handleClick={restartGame} text='Restart Game' />
+  </Wrapper>
 )
 
-export default Dashboard;
+export default Dashboard
